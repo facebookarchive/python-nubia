145cb5d972cfb1400c9280a03e72b45e616a191f
Ahmed Soliman <me@ahmedsoliman.com>
2019-04-30 11:43:28 +0100

    Introducing automatic command loading support for nubia

--- example/nubia_example.py
+++ example/nubia_example.py
@@ -10,9 +10,14 @@
 import sys
 from nubia import Nubia, Options
 from nubia_plugin import NubiaExamplePlugin
+import example.commands
 
 if __name__ == "__main__":
     plugin = NubiaExamplePlugin()
-    shell = Nubia(name="nubia_example", plugin=plugin,
-            options=Options(persistent_history=False))
+    shell = Nubia(
+        name="nubia_example",
+        command_pkgs=example.commands,
+        plugin=plugin,
+        options=Options(persistent_history=False),
+    )
     sys.exit(shell.run())
