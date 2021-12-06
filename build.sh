3a3befa4ede3faa92a3ec66a3706c2c7471f826c
Andreas Backx <andreasbackx@fb.com>
2021-12-06 12:06:30 -0800

    Added poetry and lock pyparsing to ^2.4.7. (#86)
    Summary:
    I think it's a good idea to revisit the discussion from https://github.com/facebookincubator/python-nubia/issues/20. The tools have matured and considering the current state of Nubia, this would at least fix the problems we are having. Using poetry is considerably simpler and [distutils is deprecated](https://www.python.org/dev/peps/pep-0632/).
    
    The following changes are also included:
    * All dependencies have been set to the latest version and pyparsing to `^2.4.7` as `3.x` is not supported right now.
    
    Resolves https://github.com/facebookincubator/python-nubia/issues/85.
    
    Pull Request resolved: https://github.com/facebookincubator/python-nubia/pull/86
    
    Reviewed By: AhmedSoliman
    
    Differential Revision: D32879106
    
    Pulled By: AndreasBackx
    
    fbshipit-source-id: e07de2b1eaba3a9e82a5d12493a7079297e42727

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Copyright (c) Facebook, Inc. and its affiliates.
-# All rights reserved.
-#
-# This source code is licensed under the BSD-style license found in the
-# LICENSE file in the root directory of this source tree.
-
-python3 setup.py sdist bdist_wheel
